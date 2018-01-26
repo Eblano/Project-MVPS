dfb36a859aa1253c9782f8e721d2a14fd08a2151
VadimAndriyanov <Vadim.Andriyanov@outlook.com>
2018-01-26 23:19:56 +0200

    first

--- Assets/Battlehub/RTSaveLoad2/Scripts/PersistentObject.cs
+++ Assets/Battlehub/RTSaveLoad2/Scripts/PersistentObject.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections.Generic;
 using ProtoBuf;
 using UnityEngine;
@@ -29,6 +30,29 @@ namespace Battlehub.RTSaveLoad2
             uo.hideFlags = (HideFlags)hideFlags;
             return obj;
         }       
+=======
+﻿using UnityEngine;
+
+namespace Battlehub.RTSaveLoad2
+{
+    //show wizard during project import
+    //wizard would ask to create persistent objects and field mapping objects
+    //user could choose accept predefined set of types, 
+    //selected/unselect additional types or skip generation.
+    
+    //case 1 - Fresh Install:
+    //wizard will display unity object fields to the left
+    //and matched persistent objects fields to the right
+    //case 2 - Upgrade: 
+
+    //peristent objects will use primitive types and standard .net types or persistent objects
+    //to store saved data
+
+    public class PersistentObject 
+    {
+        public string name;
+        public uint hideFlags;
+>>>>>>> bfaf5ccd... first
     }
 
 }
