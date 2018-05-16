18f4acec211dd266ff01d892395b8415e3de8c30
Bernard Chua Kim Swee <bernardcks123@gmail.com>
2018-05-16 13:27:37 +0800

    _____________READY TO MERGE WITH RTE

---
+++ Assets/TransformationSync.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TransformationSync : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (isServer)
+        {
+            RpcSyncTransform(transform.position, transform.eulerAngles);
+        }
+    }
+
+    [ClientRpc]
+    private void RpcSyncTransform(Vector3 position, Vector3 eulerAngle)
+    {
+        transform.position = position;
+        transform.rotation = Quaternion.Euler(eulerAngle);
+    }
+}
