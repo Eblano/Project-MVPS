8d1022b960263bb370183199dc8b7dabfeb3df11
Low Zhi Heng <lowzhiheng@gmail.com>
2018-06-15 18:38:16 +0800

    In progress

---
+++ Assets/_OwnAssets/_Scripts/NPCScripting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SealTeam4
+{
+    public class NPCScripting : MonoBehaviour
+    {
+        [SerializeField] private GameObject npcList;
+        [SerializeField] private GameObject npcList_Entry_Prefab;
+
+        public void Add_NPCList_Entry()
+        {
+            GameObject go = Instantiate(npcList_Entry_Prefab, Vector3.zero, Quaternion.identity);
+            go.transform.SetParent(npcList.transform);
+        }
+    }
+}
