089eab80cb7164f680614247110a6919e42d1d56
Bernard Chua <bernardcks123@gmail.com>
2018-07-23 12:28:54 +0800

    Reworked PlayerStats script

--- Assets/_Test/PlayerStats.cs
+++ Assets/_Test/PlayerStats.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 using UnityEngine.Networking;
 using SealTeam4;
 
-public class PlayerStats : MonoBehaviour
+public class PlayerStats : MonoBehaviour, IDamageable
 {
-    private int hitPoints = 100;
+    [SerializeField] private int totalHp = 100;
+    [SerializeField] private HitBoxColliders hitBoxColliders;
+
+    public int headDmg = 100;
+    public int bodyDmg = 20;
+    public int handDmg = 15;
+    public int legDmg = 15;
+
     private NetworkInstanceId netID;
+
+    [System.Serializable]
+    private class HitBoxColliders
+    {
+        public List<Collider> headColliders;
+        public List<Collider> bodyColliders;
+        public List<Collider> HandColliders;
+        public List<Collider> legColliders;
+    }
 
     private void Start()
     {
@@ -21,22 +37,61 @@ public class PlayerStats : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        hitPoints -= damage;
+        totalHp -= damage;
         UpdateOverlay();
         CheckHealth();
     }
 
     private void UpdateOverlay()
     {
-        GameManagerAssistant.instance.TargetUpdatePanelTransparency(NetworkServer.objects[netID].connectionToClient, hitPoints);
+        GameManagerAssistant.instance.TargetUpdatePanelTransparency(NetworkServer.objects[netID].connectionToClient, totalHp);
     }
 
     private void CheckHealth()
     {
-        if (hitPoints <= 0)
+        if (totalHp <= 0)
         {
             // Stop game when dead
             Time.timeScale = 0;
+        }
+    }
+
+    public void OnHit(Collider c)
+    {
+        foreach (Collider bodyColl in hitBoxColliders.bodyColliders)
+        {
+            if (c == bodyColl)
+            {
+                TakeDamage(bodyDmg);
+                return;
+            }
+        }
+
+        foreach (Collider headColl in hitBoxColliders.headColliders)
+        {
+            if (c == headColl)
+            {
+                TakeDamage(headDmg);
+                return;
+            }
+        }
+
+        foreach (Collider handColl in hitBoxColliders.HandColliders)
+        {
+            if (c == handColl)
+            {
+                TakeDamage(handDmg);
+                return;
+            }
+        }
+
+        foreach (Collider legColl in hitBoxColliders.legColliders)
+        {
+            if (c == legColl)
+            {
+                TakeDamage(handDmg);
+                return;
+            }
         }
     }
 }
