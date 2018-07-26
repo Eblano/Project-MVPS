34107bb638912a5cc3c93db68d213fa51a1f397e
Bernard Chua <bernardcks123@gmail.com>
2018-07-26 20:04:09 +0800

    Added torchlight attachment primitive

---
+++ Assets/Torchlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Torchlight : MonoBehaviour, IUsableObject
+{
+    [SerializeField] private Light lightSource;
+
+    public void UseObject(NetworkInstanceId networkInstanceId)
+    {
+        lightSource.enabled = !lightSource.enabled;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UseObject(new NetworkInstanceId(1));
+        }
+    }
+}
