421c93a8f617809756f89cd442c9398ffbb7b2a3
Low Zhi Heng <lowzhiheng@gmail.com>
2018-07-29 14:04:43 +0800

    ++ Added editor script to help with re exposing runtime editor prefab

---
+++ Assets/_OwnAssets/_Scripts/Editor/RefreshPrefab.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RefreshPrefab
+{
+    [MenuItem("Tools/Refresh Selected Prefabs")]
+    private static void RefreshSelectedPrefabs()
+    {
+        int counter = 0;
+        Object[] selectedObjects = Selection.objects;
+        
+        foreach(Object selectedObject in selectedObjects)
+        {
+            if (GameObject.Find(selectedObject.name) == true)
+            {
+                Debug.LogWarning("Existing " + selectedObject.name + " with same name is on scene, skipping");
+                continue;
+            }
+            else
+            {
+                PrefabUtility.InstantiatePrefab(selectedObject);
+
+                GameObject instantiatedGO = GameObject.Find(selectedObject.name);
+                PrefabUtility.CreatePrefab(AssetDatabase.GetAssetPath(selectedObject), instantiatedGO);
+                GameObject.Destroy(instantiatedGO);
+
+                counter++;
+            }
+        }
+
+        Debug.Log("Successfully Refreshed " + counter + " prefabs, please manually expose them to editor again");
+    }
+}
