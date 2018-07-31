123796d5bc0361b55ebe5c416992977967e0dc58
Bernard Chua <bernardcks123@gmail.com>
2018-07-31 19:15:39 +0800

    MISC ME WITH THAT GAY SHIT --Manual merging of major networking scripts

--- Assets/_OwnAssets/_Scripts/GameManagerAssistant.cs
+++ Assets/_OwnAssets/_Scripts/GameManagerAssistant.cs
@@ -9,6 +9,7 @@ namespace SealTeam4
     {
         public static GameManagerAssistant instance;
         public List<IActions> allActions;
+        public NetworkInstanceId playerID;
 
         private void Update()
         {
@@ -17,19 +18,6 @@ namespace SealTeam4
             else
                 return;
         }
-
-        // Example of sending data
-        //[Command]
-        //public void CmdSetBool(bool state)
-        //{
-        //    RpcSetBool(state);
-        //}
-
-        //[ClientRpc]
-        //public void RpcSetBool(bool state)
-        //{
-        //    GameManager.instance.UpdateNetworkTestBool(state);
-        //}
 
         [TargetRpc]
         public void TargetUpdatePanelTransparency(NetworkConnection networkConnection, int percent)
@@ -42,43 +30,11 @@ namespace SealTeam4
             NetworkServer.Spawn(GO);
         }
 
-        //[Command]
-        //public void CmdSyncHaps(NetworkInstanceId networkInstanceId, ControllerHapticsManager.HapticType hapticType, VRTK.VRTK_DeviceFinder.Devices devices)
-        //{
-        //    TargetSyncHaps(NetworkServer.objects[networkInstanceId].connectionToClient, hapticType, devices);
-        //}
-
         [Command]
         public void CmdRegisterClient(string clientName)
         {
             GameManager.instance.AddNewPlayer(clientName);
         }
-
-        //[TargetRpc]
-        //public void TargetSyncHaps(NetworkConnection networkConnection, ControllerHapticsManager.HapticType hapticType, VRTK.VRTK_DeviceFinder.Devices devices)
-        //{
-        //    ControllerHapticsManager.instance.PlayHaptic(hapticType, devices);
-        //}
-
-        // Experimental Feature
-        [ClientRpc]
-        public void RpcSyncActions(int actionNum, string actionCommand)
-        {
-            // maybe just use game find all IActions and add to a list then send over the list number
-            allActions[actionNum].SetAction(actionCommand);
-        }
-
-        [ClientRpc]
-        public void RpcGameManagerSendCommand(string goName, string msg)
-        {
-            GameManager.instance.RecieveNetCmdObjMsg(goName, msg);
-        }
-
-        //[TargetRpc]
-        //public void TargetSyncSound(NetworkConnection networkConnection, ControllerHapticsManager.HapticType hapticType, VRTK.VRTK_DeviceFinder.Devices devices)
-        //{
-        //    ControllerHapticsManager.instance.PlayHaptic(hapticType, devices);
-        //}
 
         [Command]
         public void CmdSendAudioSourceInt(int audioSourceNum)
@@ -102,15 +58,187 @@ namespace SealTeam4
         public void RpcSyncScale(NetworkInstanceId networkId, Vector3 yScale, Vector3 handScale)
         {
             Debug.Log("NetworkID: " + networkId);
-            Debug.Log("NetworkObject: " + NetworkServer.objects[networkId]);
-            Debug.Log("GO Name: " + NetworkServer.objects[networkId].gameObject.name);
+            Debug.Log("NetworkObject: " + ClientScene.objects[networkId]);
+            Debug.Log("GO Name: " + ClientScene.objects[networkId].gameObject.name);
 
-            if (!NetworkServer.objects.ContainsKey(networkId))
+            if (!ClientScene.objects.ContainsKey(networkId))
             {
                 return;
             }
-            
-            NetworkServer.objects[networkId].gameObject.GetComponent<PlayerSizeCalibration>().ApplyScale(yScale, handScale);
+
+            ClientScene.objects[networkId].gameObject.GetComponent<PlayerSizeCalibration>().ApplyScale(yScale, handScale);
         }
+
+        #region PlayerInteractions
+        // Player interaction syncs
+        public override void OnStartLocalPlayer()
+        {
+            playerID = GetComponent<NetworkIdentity>().netId;
+        }
+
+        public void RelaySenderCmdSnapToController(NetworkInstanceId childID, bool isLeftController)
+        {
+            CmdSnapToController(childID, playerID, isLeftController);
+        }
+
+        public void RelaySenderCmdUnSnapFromController(bool isLeftController, Vector3 velo, Vector3 anguVelo)
+        {
+            CmdUnSnapFromController(playerID, isLeftController, velo, anguVelo);
+        }
+
+        public void RelaySenderCmdGunShellSync(NetworkInstanceId gunNetID, Vector3 force)
+        {
+            CmdGunShellSync(playerID, gunNetID, force);
+        }
+
+        public void RelaySenderCmdGunEffectSync(NetworkInstanceId gunNetID)
+        {
+            CmdGunEffectSync(playerID, gunNetID);
+        }
+
+        public void RelaySenderCmdDropMagazine(NetworkInstanceId gunNetID)
+        {
+            CmdDropMagazine(playerID, gunNetID);
+        }
+
+        public void RelaySenderCmdTorchlightState(NetworkInstanceId torchNetID, bool lightState)
+        {
+            CmdTorchlightState(playerID, torchNetID, lightState);
+        }
+
+        [Command]
+        public void CmdSnapToParentGameObj(NetworkInstanceId childID, NetworkInstanceId parentID, Vector3 offset)
+        {
+            RpcSnapToParentGameObj(childID, parentID, offset);
+        }
+
+        [Command]
+        private void CmdSnapToController(NetworkInstanceId childID, NetworkInstanceId senderPlayerID, bool isLeftController)
+        {
+            RpcSnapToController(childID, senderPlayerID, isLeftController);
+        }
+
+        [Command]
+        private void CmdUnSnapFromController(NetworkInstanceId senderPlayerID, bool isLeftController, Vector3 velo, Vector3 anguVelo)
+        {
+            RpcUnSnapFromController(senderPlayerID, isLeftController, velo, anguVelo);
+        }
+
+        [Command]
+        private void CmdGunShellSync(NetworkInstanceId senderPlayerID, NetworkInstanceId gunNetID, Vector3 force)
+        {
+            RpcGunShellSync(senderPlayerID, gunNetID, force);
+        }
+
+        [Command]
+        private void CmdGunEffectSync(NetworkInstanceId senderPlayerID, NetworkInstanceId gunNetID)
+        {
+            RpcGunEffectSync(senderPlayerID, gunNetID);
+        }
+
+        [Command]
+        private void CmdDropMagazine(NetworkInstanceId senderPlayerID, NetworkInstanceId gunNetID)
+        {
+            RpcDropMagazine(senderPlayerID, gunNetID);
+        }
+
+        [Command]
+        private void CmdTorchlightState(NetworkInstanceId senderPlayerID, NetworkInstanceId torchNetID, bool lightState)
+        {
+            RpcTorchlightState(senderPlayerID, torchNetID, lightState);
+        }
+
+        [ClientRpc]
+        private void RpcSnapToParentGameObj(NetworkInstanceId childID, NetworkInstanceId parentID, Vector3 offset)
+        {
+            SnapTo(ClientScene.objects[childID].gameObject, ClientScene.objects[parentID].gameObject, offset);
+        }
+
+        [ClientRpc]
+        private void RpcSnapToController(NetworkInstanceId childID, NetworkInstanceId senderPlayerID, bool isLeftController)
+        {
+            ClientScene.objects[senderPlayerID].GetComponent<PlayerInteractionSync>().SyncControllerSnap(isLeftController, ClientScene.objects[childID].gameObject);
+        }
+
+        [ClientRpc]
+        private void RpcUnSnapFromController(NetworkInstanceId senderPlayerID, bool isLeftController, Vector3 velo, Vector3 anguVelo)
+        {
+            ClientScene.objects[senderPlayerID].GetComponent<PlayerInteractionSync>().SyncControllerUnSnap(isLeftController, velo, anguVelo);
+        }
+
+        [ClientRpc]
+        private void RpcGunShellSync(NetworkInstanceId senderPlayerID, NetworkInstanceId gunNetID, Vector3 force)
+        {
+            if (senderPlayerID == playerID)
+            {
+                return;
+            }
+
+            ClientScene.objects[gunNetID].GetComponent<Gun>().BulletShellSpawn(force);
+        }
+
+        [ClientRpc]
+        private void RpcGunEffectSync(NetworkInstanceId senderPlayerID, NetworkInstanceId gunNetID)
+        {
+            if (senderPlayerID == playerID)
+            {
+                return;
+            }
+
+            ClientScene.objects[gunNetID].GetComponent<Gun>().ActivateGunEffects();
+        }
+
+        [ClientRpc]
+        private void RpcDropMagazine(NetworkInstanceId senderPlayerID, NetworkInstanceId gunNetID)
+        {
+            if (senderPlayerID == playerID)
+            {
+                return;
+            }
+
+            ClientScene.objects[gunNetID].GetComponent<Gun>().UnloadMagazine();
+        }
+
+        [ClientRpc]
+        private void RpcTorchlightState(NetworkInstanceId senderPlayerID, NetworkInstanceId torchNetID, bool lightState)
+        {
+            if (senderPlayerID == playerID)
+            {
+                return;
+            }
+
+            ClientScene.objects[torchNetID].GetComponent<Torchlight>().SetLightState(lightState);
+        }
+
+        [Command]
+        public void CmdGunFire(NetworkInstanceId gunNetID)
+        {
+            NetworkServer.objects[gunNetID].GetComponent<Gun>().FireBullet();
+        }
+
+        private void SnapTo(GameObject child, GameObject parent)
+        {
+            if (child.GetComponent<Rigidbody>())
+            {
+                child.GetComponent<Rigidbody>().isKinematic = true;
+            }
+
+            child.transform.SetParent(parent.transform);
+            child.transform.localPosition = Vector3.zero;
+            child.transform.localRotation = Quaternion.identity;
+        }
+
+        private void SnapTo(GameObject child, GameObject parent, Vector3 offset)
+        {
+            if (child.GetComponent<Rigidbody>())
+            {
+                child.GetComponent<Rigidbody>().isKinematic = true;
+            }
+
+            child.transform.SetParent(parent.transform);
+            child.transform.localPosition = -offset;
+            child.transform.localRotation = Quaternion.identity;
+        }
+        #endregion PlayerInteractions
     }
 }
--- Assets/_OwnAssets/_Test/___WeirdScripts/WeirdPlayerInput.cs
+++ Assets/_OwnAssets/_Test/___WeirdScripts/WeirdPlayerInput.cs
@@ -1,202 +1,202 @@
-﻿using UnityEngine;
-using UnityEngine.Networking;
-using VRTK;
-using System.Collections;
+﻿//using UnityEngine;
+//using UnityEngine.Networking;
+//using VRTK;
+//using System.Collections;
 
-namespace SealTeam4
-{
-    public class WeirdPlayerInput : MonoBehaviour
-    {
-        private VRTK_ControllerEvents lHandEvents;
-        private VRTK_ControllerEvents rHandEvents;
-        public static VRTK_ControllerReference lHandRef;
-        public static VRTK_ControllerReference rHandRef;
-        private WeirdPlayerInteractionSync playerInteractionSync;
+//namespace SealTeam4
+//{
+//    public class WeirdPlayerInput : MonoBehaviour
+//    {
+//        private VRTK_ControllerEvents lHandEvents;
+//        private VRTK_ControllerEvents rHandEvents;
+//        public static VRTK_ControllerReference lHandRef;
+//        public static VRTK_ControllerReference rHandRef;
+//        private WeirdPlayerInteractionSync playerInteractionSync;
 
-        private Transform headset, lHandCont, rHandCont;
-        private Vector3AndQuaternion head, lHand, rHand;
+//        private Transform headset, lHandCont, rHandCont;
+//        private Vector3AndQuaternion head, lHand, rHand;
 
-        private NetworkIdentity networkIdentity;
+//        private NetworkIdentity networkIdentity;
 
-        [SerializeField] private float grabRadius;
-        [SerializeField] private float holdTouchPadTimer;
+//        [SerializeField] private float grabRadius;
+//        [SerializeField] private float holdTouchPadTimer;
 
-        [SerializeField] private float sendRatePerSecond = 30;
-        private float counter = float.MinValue;
+//        [SerializeField] private float sendRatePerSecond = 30;
+//        private float counter = float.MinValue;
 
-        private float leftTimer;
-        private float rightTimer;
+//        private float leftTimer;
+//        private float rightTimer;
 
-        private void Start()
-        {
-            networkIdentity = GetComponent<NetworkIdentity>();
-            string playerName = "Player " + networkIdentity.netId;
-            gameObject.name = playerName;
+//        private void Start()
+//        {
+//            networkIdentity = GetComponent<NetworkIdentity>();
+//            string playerName = "Player " + networkIdentity.netId;
+//            gameObject.name = playerName;
 
-            playerInteractionSync = GetComponent<WeirdPlayerInteractionSync>();
-            if (!playerInteractionSync.isLocalPlayer)
-            {
-                Destroy(this);
-                return;
-            }
+//            playerInteractionSync = GetComponent<WeirdPlayerInteractionSync>();
+//            if (!playerInteractionSync.isLocalPlayer)
+//            {
+//                Destroy(this);
+//                return;
+//            }
 
-            //GameManager.instance.SetLocalPlayerName(playerName);
+//            //GameManager.instance.SetLocalPlayerName(playerName);
 
-            head = new Vector3AndQuaternion();
-            lHand = new Vector3AndQuaternion();
-            rHand = new Vector3AndQuaternion();
-            headset = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
-            lHandCont = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.LeftController);
-            rHandCont = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.RightController);
+//            head = new Vector3AndQuaternion();
+//            lHand = new Vector3AndQuaternion();
+//            rHand = new Vector3AndQuaternion();
+//            headset = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
+//            lHandCont = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.LeftController);
+//            rHandCont = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.RightController);
 
-            lHandEvents = lHandCont.GetComponent<VRTK_ControllerEvents>();
-            rHandEvents = rHandCont.GetComponent<VRTK_ControllerEvents>();
+//            lHandEvents = lHandCont.GetComponent<VRTK_ControllerEvents>();
+//            rHandEvents = rHandCont.GetComponent<VRTK_ControllerEvents>();
 
-            StartCoroutine(SetController());
+//            StartCoroutine(SetController());
 
-            lHandEvents.GripClicked += LHandEvents_GripClicked;
-            rHandEvents.GripClicked += RHandEvents_GripClicked;
+//            lHandEvents.GripClicked += LHandEvents_GripClicked;
+//            rHandEvents.GripClicked += RHandEvents_GripClicked;
 
-            lHandEvents.GripUnclicked += LHandEvents_GripUnclicked;
-            rHandEvents.GripUnclicked += RHandEvents_GripUnclicked;
+//            lHandEvents.GripUnclicked += LHandEvents_GripUnclicked;
+//            rHandEvents.GripUnclicked += RHandEvents_GripUnclicked;
 
-            lHandEvents.TriggerClicked += LHandEvents_TriggerClicked;
-            rHandEvents.TriggerClicked += RHandEvents_TriggerClicked;
+//            lHandEvents.TriggerClicked += LHandEvents_TriggerClicked;
+//            rHandEvents.TriggerClicked += RHandEvents_TriggerClicked;
 
-            lHandEvents.TouchpadPressed += LHandEvents_TouchpadPressed;
-            rHandEvents.TouchpadPressed += RHandEvents_TouchpadPressed;
+//            lHandEvents.TouchpadPressed += LHandEvents_TouchpadPressed;
+//            rHandEvents.TouchpadPressed += RHandEvents_TouchpadPressed;
 
-            lHandEvents.TouchpadReleased += LHandEvents_TouchpadReleased;
-            rHandEvents.TouchpadReleased += RHandEvents_TouchpadReleased;
-        }
+//            lHandEvents.TouchpadReleased += LHandEvents_TouchpadReleased;
+//            rHandEvents.TouchpadReleased += RHandEvents_TouchpadReleased;
+//        }
 
-        private void FixedUpdate()
-        {
-            head.SetPosAndRot(headset);
-            lHand.SetPosAndRot(lHandCont);
-            rHand.SetPosAndRot(rHandCont);
+//        private void FixedUpdate()
+//        {
+//            head.SetPosAndRot(headset);
+//            lHand.SetPosAndRot(lHandCont);
+//            rHand.SetPosAndRot(rHandCont);
 
-            playerInteractionSync.UpdateLocal(head, lHand, rHand);
+//            playerInteractionSync.UpdateLocal(head, lHand, rHand);
 
-            if (IsCounterReady(ref counter, 1 / sendRatePerSecond))
-            {
-                playerInteractionSync.CmdSyncVRTransform(head, lHand, rHand);
-            }
-        }
+//            if (IsCounterReady(ref counter, 1 / sendRatePerSecond))
+//            {
+//                playerInteractionSync.CmdSyncVRTransform(head, lHand, rHand);
+//            }
+//        }
 
-        private bool IsCounterReady(ref float timeCounter, float downTime)
-        {
-            if (Time.time > timeCounter)
-            {
-                timeCounter = Time.time + downTime;
-                return true;
-            }
+//        private bool IsCounterReady(ref float timeCounter, float downTime)
+//        {
+//            if (Time.time > timeCounter)
+//            {
+//                timeCounter = Time.time + downTime;
+//                return true;
+//            }
 
-            return false;
-        }
+//            return false;
+//        }
 
-        IEnumerator SetController()
-        {
-            while (!VRTK_ControllerReference.IsValid(lHandRef) || !VRTK_ControllerReference.IsValid(rHandRef))
-            {
-                lHandRef = VRTK_DeviceFinder.GetControllerReferenceLeftHand();
-                rHandRef = VRTK_DeviceFinder.GetControllerReferenceRightHand();
+//        IEnumerator SetController()
+//        {
+//            while (!VRTK_ControllerReference.IsValid(lHandRef) || !VRTK_ControllerReference.IsValid(rHandRef))
+//            {
+//                lHandRef = VRTK_DeviceFinder.GetControllerReferenceLeftHand();
+//                rHandRef = VRTK_DeviceFinder.GetControllerReferenceRightHand();
 
-                yield return new WaitForSeconds(0.5f);
-            }
+//                yield return new WaitForSeconds(0.5f);
+//            }
 
-            Debug.Log(lHandRef);
-            Debug.Log(rHandRef);
-        }
+//            Debug.Log(lHandRef);
+//            Debug.Log(rHandRef);
+//        }
 
-        private void RHandEvents_TriggerClicked(object sender, ControllerInteractionEventArgs e)
-        {
-            playerInteractionSync.TriggerClick(VRTK_DeviceFinder.Devices.RightController, networkIdentity.netId);
-        }
+//        private void RHandEvents_TriggerClicked(object sender, ControllerInteractionEventArgs e)
+//        {
+//            playerInteractionSync.TriggerClick(VRTK_DeviceFinder.Devices.RightController, networkIdentity.netId);
+//        }
 
-        private void LHandEvents_TriggerClicked(object sender, ControllerInteractionEventArgs e)
-        {
-            playerInteractionSync.TriggerClick(VRTK_DeviceFinder.Devices.LeftController, networkIdentity.netId);
-        }
+//        private void LHandEvents_TriggerClicked(object sender, ControllerInteractionEventArgs e)
+//        {
+//            playerInteractionSync.TriggerClick(VRTK_DeviceFinder.Devices.LeftController, networkIdentity.netId);
+//        }
 
-        private void RHandEvents_GripUnclicked(object sender, ControllerInteractionEventArgs e)
-        {
-            playerInteractionSync.Ungrab(VRTK_DeviceFinder.Devices.RightController, VRTK_DeviceFinder.GetControllerVelocity(rHandRef), VRTK_DeviceFinder.GetControllerAngularVelocity(rHandRef));
-        }
+//        private void RHandEvents_GripUnclicked(object sender, ControllerInteractionEventArgs e)
+//        {
+//            playerInteractionSync.Ungrab(VRTK_DeviceFinder.Devices.RightController, VRTK_DeviceFinder.GetControllerVelocity(rHandRef), VRTK_DeviceFinder.GetControllerAngularVelocity(rHandRef));
+//        }
 
-        private void LHandEvents_GripUnclicked(object sender, ControllerInteractionEventArgs e)
-        {
-            playerInteractionSync.Ungrab(VRTK_DeviceFinder.Devices.LeftController, VRTK_DeviceFinder.GetControllerVelocity(lHandRef), VRTK_DeviceFinder.GetControllerAngularVelocity(lHandRef));
-        }
+//        private void LHandEvents_GripUnclicked(object sender, ControllerInteractionEventArgs e)
+//        {
+//            playerInteractionSync.Ungrab(VRTK_DeviceFinder.Devices.LeftController, VRTK_DeviceFinder.GetControllerVelocity(lHandRef), VRTK_DeviceFinder.GetControllerAngularVelocity(lHandRef));
+//        }
 
-        private void RHandEvents_GripClicked(object sender, ControllerInteractionEventArgs e)
-        {
-            playerInteractionSync.Grab(VRTK_DeviceFinder.Devices.RightController, grabRadius);
-        }
+//        private void RHandEvents_GripClicked(object sender, ControllerInteractionEventArgs e)
+//        {
+//            playerInteractionSync.Grab(VRTK_DeviceFinder.Devices.RightController, grabRadius);
+//        }
 
-        private void LHandEvents_GripClicked(object sender, ControllerInteractionEventArgs e)
-        {
-            playerInteractionSync.Grab(VRTK_DeviceFinder.Devices.LeftController, grabRadius);
-        }
+//        private void LHandEvents_GripClicked(object sender, ControllerInteractionEventArgs e)
+//        {
+//            playerInteractionSync.Grab(VRTK_DeviceFinder.Devices.LeftController, grabRadius);
+//        }
 
-        private void RHandEvents_TouchpadPressed(object sender, ControllerInteractionEventArgs e)
-        {
-            playerInteractionSync.TouchpadButton(VRTK_DeviceFinder.Devices.RightController, rHandEvents.GetTouchpadAxis());
-            TouchPadDown(VRTK_DeviceFinder.Devices.RightController);
-        }
+//        private void RHandEvents_TouchpadPressed(object sender, ControllerInteractionEventArgs e)
+//        {
+//            playerInteractionSync.TouchpadButton(VRTK_DeviceFinder.Devices.RightController, rHandEvents.GetTouchpadAxis());
+//            TouchPadDown(VRTK_DeviceFinder.Devices.RightController);
+//        }
 
-        private void LHandEvents_TouchpadPressed(object sender, ControllerInteractionEventArgs e)
-        {
-            playerInteractionSync.TouchpadButton(VRTK_DeviceFinder.Devices.LeftController, lHandEvents.GetTouchpadAxis());
-            TouchPadDown(VRTK_DeviceFinder.Devices.LeftController);
-        }
+//        private void LHandEvents_TouchpadPressed(object sender, ControllerInteractionEventArgs e)
+//        {
+//            playerInteractionSync.TouchpadButton(VRTK_DeviceFinder.Devices.LeftController, lHandEvents.GetTouchpadAxis());
+//            TouchPadDown(VRTK_DeviceFinder.Devices.LeftController);
+//        }
 
-        private void RHandEvents_TouchpadReleased(object sender, ControllerInteractionEventArgs e)
-        {
-            TouchPadUp(VRTK_DeviceFinder.Devices.RightController);
-        }
+//        private void RHandEvents_TouchpadReleased(object sender, ControllerInteractionEventArgs e)
+//        {
+//            TouchPadUp(VRTK_DeviceFinder.Devices.RightController);
+//        }
 
-        private void LHandEvents_TouchpadReleased(object sender, ControllerInteractionEventArgs e)
-        {
-            TouchPadUp(VRTK_DeviceFinder.Devices.LeftController);
-        }
+//        private void LHandEvents_TouchpadReleased(object sender, ControllerInteractionEventArgs e)
+//        {
+//            TouchPadUp(VRTK_DeviceFinder.Devices.LeftController);
+//        }
 
-        private void TouchPadDown(VRTK_DeviceFinder.Devices control)
-        {
-            switch (control)
-            {
-                case VRTK_DeviceFinder.Devices.LeftController:
-                    leftTimer = Time.time + holdTouchPadTimer;
-                    break;
-                case VRTK_DeviceFinder.Devices.RightController:
-                    rightTimer = Time.time + holdTouchPadTimer;
-                    break;
-            }
-        }
+//        private void TouchPadDown(VRTK_DeviceFinder.Devices control)
+//        {
+//            switch (control)
+//            {
+//                case VRTK_DeviceFinder.Devices.LeftController:
+//                    leftTimer = Time.time + holdTouchPadTimer;
+//                    break;
+//                case VRTK_DeviceFinder.Devices.RightController:
+//                    rightTimer = Time.time + holdTouchPadTimer;
+//                    break;
+//            }
+//        }
 
-        private void TouchPadUp(VRTK_DeviceFinder.Devices control)
-        {
-            switch (control)
-            {
-                //case VRTK_DeviceFinder.Devices.LeftController:
-                //    // Checks if user held down button for long enough
-                //    if (Time.time > leftTimer)
-                //    {
-                //        Debug.Log("ZH DIED");
-                //        //GameManager.instance.CmdAddCalibrationPoint(gameObject.name, lHandCont.position);
-                //        NetworkPlayerPosManager.localInstance.CmdAddCalibrationPointToGameManager(gameObject.name, lHandCont.position);
-                //    }
-                //    break;
-                //case VRTK_DeviceFinder.Devices.RightController:
-                //    // Checks if user held down button for long enough
-                //    if (Time.time > rightTimer)
-                //    {
-                //        Debug.Log("ZH MUM DIED");
-                //        //GameManager.instance.CmdAddCalibrationPoint(gameObject.name, rHandCont.position);
-                //        NetworkPlayerPosManager.localInstance.CmdAddCalibrationPointToGameManager(gameObject.name, rHandCont.position);
-                //    }
-                //    break;
-            }
-        }
-    }
-}
+//        private void TouchPadUp(VRTK_DeviceFinder.Devices control)
+//        {
+//            switch (control)
+//            {
+//                //case VRTK_DeviceFinder.Devices.LeftController:
+//                //    // Checks if user held down button for long enough
+//                //    if (Time.time > leftTimer)
+//                //    {
+//                //        Debug.Log("ZH DIED");
+//                //        //GameManager.instance.CmdAddCalibrationPoint(gameObject.name, lHandCont.position);
+//                //        NetworkPlayerPosManager.localInstance.CmdAddCalibrationPointToGameManager(gameObject.name, lHandCont.position);
+//                //    }
+//                //    break;
+//                //case VRTK_DeviceFinder.Devices.RightController:
+//                //    // Checks if user held down button for long enough
+//                //    if (Time.time > rightTimer)
+//                //    {
+//                //        Debug.Log("ZH MUM DIED");
+//                //        //GameManager.instance.CmdAddCalibrationPoint(gameObject.name, rHandCont.position);
+//                //        NetworkPlayerPosManager.localInstance.CmdAddCalibrationPointToGameManager(gameObject.name, rHandCont.position);
+//                //    }
+//                //    break;
+//            }
+//        }
+//    }
+//}
