8b86fa22b8001f81f26646f27520fd8dfeb7e984
Low Zhi Heng <lowzhiheng@gmail.com>
2018-08-02 12:30:01 +0800

    Added click sfx

---
+++ Assets/KeySFXManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SealTeam4
+{
+    public class KeySFXManager : MonoBehaviour
+    {
+        public static KeySFXManager instance;
+        [SerializeField] AudioSource audioS;
+
+        private void Start()
+        {
+            if (!instance)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0) && audioS)
+                audioS.Play();
+        }
+    }
+}
